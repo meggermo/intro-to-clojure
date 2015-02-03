1789ba386546c915612c97a42de328e1bf77f6c7
Michiel Eggermont <michiel.eggermont@gmail.com>
2015-02-03 21:39:36 +0100

    Mutilmethods finished

--- src/intro_to_clojure/multimethods.clj
+++ src/intro_to_clojure/multimethods.clj
@@ -1,3 +1,51 @@
 (ns intro-to-clojure.multimethods)
 
-(defmulti quux)
+;; Polymorphism with multimethods
+
+;; It will dispatch on the keyword :os
+(defmulti home :os)
+(defmethod home ::unix
+  [m] (:home m))
+
+(defmulti compiler :os)
+(defmethod compiler ::unix
+  [m] (:compiler "cc"))
+
+(def unix-env
+  {:os ::unix, :home "/home", :compiler "cc"})
+(def os-x-env
+  {:os ::os-x, :home "/Users", :compiler "gcc"})
+
+;; Now we can call the multimethod
+(home unix-env)
+(home os-x-env)
+;; We can fix it by letting it derive from unix
+(derive ::os-x ::unix)
+(home os-x-env)
+
+;; Suppose we add another type into the mix
+(derive ::os-x ::bsd)
+(defmethod home ::bsd [m] (:home m))
+;; Now there are two methods to choose from
+(home os-x-env)
+(parents ::os-x)
+(descendants ::unix)
+(descendants ::bsd)
+;; We can fix it by declaring an ordering preference
+(prefer-method home ::unix ::bsd)
+(home os-x-env)
+
+;; But ypu can also dispatch on any other function
+(defmulti do-something #(get-in % [:left :v]))
+(defmethod do-something 1
+  [m] (get-in m [:right :v]))
+(defmethod do-something 2
+  [m] (get-in m [:left :left :v]))
+
+(def t1
+  {:v 0 :left {:v 1} :right {:v "dispactched on left value 1"}})
+(def t2
+  {:v 0 :left {:v 2 :left {:v "dispatched on left value 2"}}})
+
+(do-something t1)
+(do-something t2)

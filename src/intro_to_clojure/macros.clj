8e158617dd2ab74e54caa403c05ccdc8bf1e67f8
Michiel Eggermont <michiel.eggermont@gmail.com>
2015-02-05 17:53:58 +0100

    with-resource macro added

--- src/intro_to_clojure/macros.clj
+++ src/intro_to_clojure/macros.clj
@@ -1,5 +1,7 @@
 (ns intro-to-clojure.macros
-  (:require [clojure.walk :as walk]))
+  (:require [clojure.walk :as walk])
+  (:import [java.io BufferedReader InputStreamReader]
+           [java.net URL]))
 
 ;; Rules of macro-club
 ;; * Don't write a macro if a function will do
@@ -13,12 +15,44 @@
 (eval (list (symbol "+") 1 2))
 
 (defmacro do-until
+  "Continues evaluating pred, expression pairs until
+   the first pred returns a falsey value"
   [& clauses]
   (when clauses
     (list `when (first clauses)
-          (if (next clauses)
-            (second clauses)
-            (throw (IllegalArgumentException. "do-until requres an even number of forms")))
-          (cons 'do-until (nnext clauses)))))
+        (if (next clauses)
+          (second clauses)
+          (throw (IllegalArgumentException. "do-until requres an even number of forms")))
+        (cons 'do-until (nnext clauses)))))
 
-(macroexpand-1 '(do-until true (prn 1) false (prn 2)))
+(defn example []
+  (do-until
+   true  (prn "Yep!")
+   []    (prn "Still true!")
+   :x    (prn "And again true!")
+   false (prn "Nope, won't print")
+   true  (prn "Anything after the first falsey value won't be printed")))
+(example)
+(macroexpand
+ '(do-until true 1 false 2))
+(walk/macroexpand-all
+ '(do-until true 1 true 2 false 3 false 4))
+
+
+(defmacro with-resource
+  [binding close-fn & body]
+  `(let ~binding
+     (try
+       (do ~@body)
+       (finally
+        (~close-fn ~(binding 0))))))
+
+(defn url-stream [url]
+  (-> url URL. .openStream InputStreamReader. BufferedReader.))
+
+
+(let [stream (url-stream "http://www.joyofclojure.com/")]
+  (with-resource
+    [page stream]
+    #(.close %)
+    (.readLine page)))
